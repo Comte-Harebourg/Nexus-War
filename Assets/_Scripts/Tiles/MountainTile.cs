52fa1d5f5f9fe2ffb0b802e0448a72850ef8e9a3
Comte-Harebourg <mooreawilliam23@gmail.com>
2026-04-09 15:58:35 +0200

    Ajout du sprite dynamique de montagne

--- Assets/_Scripts/Tiles/MountainTile.cs
+++ Assets/_Scripts/Tiles/MountainTile.cs
@@ -1,6 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MountainTile : Tile
 {
-    
+    [Header("Sprites")]
+    public SpriteRenderer SpriteRenderer;
+    public Sprite Mountain1;
+    public Sprite Mountain2;
+    public Sprite Mountain3;
+    public Sprite Mountain4;
+    public Sprite Mountain5;
+    public Sprite Mountain6;
+
+    public void Start()
+    {
+        UpdateSprite();
+    }
+
+    public void UpdateSprite()
+    {
+        List<Sprite> Liste = new List<Sprite>();
+        Liste.Add(Mountain1);
+        Liste.Add(Mountain2);
+        Liste.Add(Mountain3);
+        Liste.Add(Mountain4);
+        Liste.Add(Mountain5);
+        Liste.Add(Mountain6);
+        SpriteRenderer.sprite = Liste[Random.Range(0, Liste.Count)];
+    }
 }
